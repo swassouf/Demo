9358e2609f739e28d9b7332651ccd0a56453e531
samwassouf@curo.com <samwassouf@curo.com>
2019-02-27 20:28:18 -0600

    Remove unused code

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -12,33 +12,6 @@ using Microsoft.Extensions.Options;
 
 namespace IdentityServer
 {
-    //public class Startup
-    //{
-    //    public Startup(IConfiguration configuration)
-    //    {
-    //        Configuration = configuration;
-    //    }
-
-    //    public IConfiguration Configuration { get; }
-
-    //    // This method gets called by the runtime. Use this method to add services to the container.
-    //    public void ConfigureServices(IServiceCollection services)
-    //    {
-    //        services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-    //    }
-
-    //    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    //    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-    //    {
-    //        if (env.IsDevelopment())
-    //        {
-    //            app.UseDeveloperExceptionPage();
-    //        }
-
-    //        app.UseMvc();
-    //    }
-    //}
-
     public class Startup
     {
         public IHostingEnvironment Environment { get; }
@@ -50,9 +23,6 @@ namespace IdentityServer
 
         public void ConfigureServices(IServiceCollection services)
         {
-            // uncomment, if you wan to add an MVC-based UI
-            //services.AddMvc().SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_2_1);
-
             var builder = services.AddIdentityServer()
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApis())
@@ -76,13 +46,8 @@ namespace IdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            // uncomment if you want to support static files
-            //app.UseStaticFiles();
-
             app.UseIdentityServer();
 
-            // uncomment, if you wan to add an MVC-based UI
-            //app.UseMvcWithDefaultRoute();
         }
     }
 }
